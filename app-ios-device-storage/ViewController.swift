069adae2e71804a7b454996053ca464fcdf3aa16
Aphirat Nimanussonkul <tata@odds.team>
2020-02-17 11:04:50 +0700

    added default value in table view, defined indentifier of table view cell

--- app-ios-device-storage/ViewController.swift
+++ app-ios-device-storage/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
