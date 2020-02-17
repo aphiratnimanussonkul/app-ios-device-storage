9bbae80864ee8bda9a393fe433796634481420e2
Aphirat Nimanussonkul <tata@odds.team>
2020-02-17 10:51:38 +0700

    Initial Commit

---
+++ app-ios-device-storage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-ios-device-storage
+//
+//  Created by ODDS on 17/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
